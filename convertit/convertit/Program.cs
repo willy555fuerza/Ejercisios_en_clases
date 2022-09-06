02aeaac8c73c6bb3292d03c481c7b3746a923b9b
willy <willangongales@gmail.com>
2022-09-06 13:26:07 -0400

    Ejercisios_en_clases_2

---
+++ convertit/convertit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            String c = "";
+            int[] vec = new int[10];
+            Console.Write("Tamaño de numero:");
+            5n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sus numero");
+            for (int i = 0; i < n; i++)
+            {
+                vec[i] = int.Parse(Console.ReadLine());
+
+            }
+            c = vec[1].ToString()+ vec[0].ToString();
+
+            Console.WriteLine("El elemento volteado es:" + c);
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ numero inverso/numero inverso/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numero_inverso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            int n, r, ni;
+
+            //Solicitar el valor de entrada
+            Console.WriteLine("Escribe un numero de 5 cifras: ");
+            n = int.Parse(Console.ReadLine());
+
+            //Proceso
+            r = n % 10;
+            n = n / 10;
+            ni = r * 10;
+
+            r = n % 10;
+            n = n / 10;
+            ni = (ni + r) * 10;
+
+            r = n % 10;
+            n = n / 10;
+            ni = (ni + r) * 10;
+
+            r = n % 10;
+            n = n / 10;
+            ni = (ni + r) * 10;
+
+            ni = ni + n;
+
+            //salida
+            Console.WriteLine("Numero inverso: " + ni);
+            Console.ReadKey();
+        }
+    }
+}
